85a5256d2c572f67cc102e58ed5ca5afa9e37953
bluepark83 <bluepark83@gmail.com>
2024-11-02 00:36:30 +0900

    build - scene caluelate

--- Assets/Editor/BuildRunner.Addressable.cs
+++ Assets/Editor/BuildRunner.Addressable.cs
@@ -1,10 +1,14 @@
 using UnityEditor;
 using UnityEditor.AddressableAssets.Settings;
+using UnityEditor.Build.Content;
+using UnityEngine;
 
 public partial class BuildRunner
 {
     public static bool BuildAddressables()
     {
+        Debug.Log($"BuildAddressables");
+            
         AddressableAssetSettings.BuildPlayerContent(out var result);
         return true;
     }
---
+++ Assets/Editor/ProcessSceneWithReport.cs
@@ -0,0 +1,23 @@
+using UnityEditor.Build;
+using UnityEditor.Build.Content;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Editor
+{
+    public class ProcessSceneWithReport : IProcessSceneWithReport
+    {
+        private IProcessSceneWithReport _processSceneWithReportImplementation;
+        public int callbackOrder => _processSceneWithReportImplementation.callbackOrder;
+
+        public void OnProcessScene(Scene scene, BuildReport report)
+        {
+            Debug.Log($"OnProcessScene : {scene.name}");
+            ContentBuildInterface.CalculatePlayerDependenciesForScene(scene.name, new BuildSettings(),
+                new BuildUsageTagSet(), new BuildUsageCache());
+            
+            _processSceneWithReportImplementation.OnProcessScene(scene, report);
+        }
+    }
+}
