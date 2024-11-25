9cc014624f29a11275f5bc96147478d25c0638e5
bluepark83 <bluepark83@gmail.com>
2024-11-26 02:50:25 +0900

    빌드 오류 수정

--- Assets/Editor/CalculateSceneDependencyData2.cs
+++
@@ -1,9 +0,0 @@
-using UnityEditor.Build.Pipeline.Tasks;
-
-namespace Editor
-{
-    public class CalculateSceneDependencyData2 : CalculateSceneDependencyData
-    {
-        
-    }
-}
--- Assets/Editor/SceneBundleWriteOperation2.cs
+++
@@ -1,9 +0,0 @@
-using UnityEditor.Build.Pipeline.WriteTypes;
-
-namespace Editor
-{
-    public class SceneBundleWriteOperation2 : SceneBundleWriteOperation
-    {
-        
-    }
-}
